7801b19e8e1cd79ee2f4ef88b1a1595d3e49a71b
Luis Fernando santa <l.santa@uniandes.edu.co>
2024-03-26 08:29:39 -0500

    add test

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,14 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
@@ -37,15 +28,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+    //
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,11 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('test', async ({ page }) => {
+  await page.goto('https://www.mercadolibre.com/');
+  await page.getByRole('link', { name: 'Colombia' }).click();
+  await page.getByPlaceholder('Buscar productos, marcas y má').click();
+  await page.getByPlaceholder('Buscar productos, marcas y má').fill('iphone');
+  await page.getByPlaceholder('Buscar productos, marcas y má').press('Enter');
+  await page.getByRole('link', { name: 'Apple iPhone 13 (128 GB) - Azul - Distribuidor autorizado' }).click();
+  await page.getByRole('button', { name: 'Comprar ahora' }).click();
 });
